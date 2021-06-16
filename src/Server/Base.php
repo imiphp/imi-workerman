1a96d477c1bc9e1660182610883b5b7c6469563d
Yurun <admin@yurunsoft.com>
2021-06-16 14:04:11 +0800

    重构服务器工具类，全局单例改为服务器单例

---
+++ src/Server/Base.php
@@ -0,0 +1,374 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Imi\Workerman\Server;
+
+use Channel\Client;
+use GatewayWorker\Lib\Gateway;
+use Imi\App;
+use Imi\Config;
+use Imi\Event\Event;
+use Imi\RequestContext;
+use Imi\Server\Contract\BaseServer;
+use Imi\Server\Group\Contract\IServerGroup;
+use Imi\Server\Group\TServerGroup;
+use Imi\Server\ServerManager;
+use Imi\Util\Imi;
+use Imi\Util\Socket\IPEndPoint;
+use Imi\Worker as ImiWorker;
+use Imi\Workerman\Server\Contract\IWorkermanServer;
+use InvalidArgumentException;
+use Workerman\Connection\ConnectionInterface;
+use Workerman\Connection\TcpConnection;
+use Workerman\Worker;
+
+abstract class Base extends BaseServer implements IWorkermanServer, IServerGroup
+{
+    use TServerGroup;
+
+    /**
+     * Workerman Worker 对象
+     */
+    protected Worker $worker;
+
+    /**
+     * Workerman Worker 类名.
+     */
+    protected string $workerClass = WorkermanServerWorker::class;
+
+    /**
+     * 构造方法.
+     */
+    public function __construct(string $name, array $config)
+    {
+        parent::__construct($name, $config);
+        $this->worker = $this->createServer();
+        $this->bindEvents();
+    }
+
+    protected function createServer(): Worker
+    {
+        $config = $this->config;
+        if (isset($config['worker']))
+        {
+            $class = $this->workerClass = $config['worker'];
+        }
+        else
+        {
+            $class = $this->workerClass;
+        }
+        if (isset($config['socketName']))
+        {
+            $socketName = $config['socketName'];
+        }
+        elseif (isset($config['host'], $config['port']))
+        {
+            $socketName = $this->getWorkerScheme() . '://' . $config['host'] . ':' . $config['port'];
+        }
+        else
+        {
+            $socketName = '';
+        }
+        $worker = new $class($socketName, $config['context'] ?? []);
+        $worker->name = $this->name;
+        foreach ($config['configs'] ?? [] as $k => $v)
+        {
+            $worker->$k = $v;
+        }
+
+        return $worker;
+    }
+
+    /**
+     * 是否支持 SSL.
+     */
+    public function isSSL(): bool
+    {
+        return isset($this->worker) && 'ssl' === $this->worker->transport;
+    }
+
+    /**
+     * 获取 Workerman Worker 对象
+     */
+    public function getWorker(): Worker
+    {
+        return $this->worker;
+    }
+
+    /**
+     * 开启服务
+     */
+    public function start(): void
+    {
+    }
+
+    /**
+     * 终止服务
+     */
+    public function shutdown(): void
+    {
+        Worker::stopAll();
+    }
+
+    /**
+     * 重载服务
+     */
+    public function reload(): void
+    {
+        Worker::reloadAllWorkers();
+    }
+
+    /**
+     * 绑定服务器事件.
+     */
+    protected function bindEvents(): void
+    {
+        $this->worker->onBufferDrain = function (ConnectionInterface $connection) {
+            try
+            {
+                // @phpstan-ignore-next-line
+                $clientId = $connection->id;
+                RequestContext::muiltiSet([
+                    'server'   => $this,
+                    'clientId' => $clientId,
+                ]);
+                Event::trigger('IMI.WORKERMAN.SERVER.BUFFER_DRAIN', [
+                    'server'     => $this,
+                    'clientId'   => $clientId,
+                    'connection' => $connection,
+                ], $this);
+                RequestContext::destroy();
+            }
+            catch (\Throwable $ex)
+            {
+                App::getBean('ErrorLog')->onException($ex);
+            }
+        };
+
+        $this->worker->onBufferFull = function (ConnectionInterface $connection) {
+            try
+            {
+                // @phpstan-ignore-next-line
+                $clientId = $connection->id;
+                RequestContext::muiltiSet([
+                    'server'   => $this,
+                    'clientId' => $clientId,
+                ]);
+                Event::trigger('IMI.WORKERMAN.SERVER.BUFFER_FULL', [
+                    'server'     => $this,
+                    'clientId'   => $clientId,
+                    'connection' => $connection,
+                ], $this);
+                RequestContext::destroy();
+            }
+            catch (\Throwable $ex)
+            {
+                App::getBean('ErrorLog')->onException($ex);
+            }
+        };
+
+        $this->worker->onClose = function (ConnectionInterface $connection) {
+            try
+            {
+                // @phpstan-ignore-next-line
+                $clientId = $connection->id;
+                RequestContext::muiltiSet([
+                    'server'   => $this,
+                    'clientId' => $clientId,
+                ]);
+                Event::trigger('IMI.WORKERMAN.SERVER.CLOSE', [
+                    'server'     => $this,
+                    'clientId'   => $clientId,
+                    'connection' => $connection,
+                ], $this);
+                RequestContext::destroy();
+            }
+            catch (\Throwable $ex)
+            {
+                App::getBean('ErrorLog')->onException($ex);
+            }
+        };
+
+        $this->worker->onConnect = function (ConnectionInterface $connection) {
+            try
+            {
+                // @phpstan-ignore-next-line
+                $clientId = $connection->id;
+                RequestContext::muiltiSet([
+                    'server'   => $this,
+                    'clientId' => $clientId,
+                ]);
+                Event::trigger('IMI.WORKERMAN.SERVER.CONNECT', [
+                    'server'     => $this,
+                    'clientId'   => $clientId,
+                    'connection' => $connection,
+                ], $this);
+                RequestContext::destroy();
+            }
+            catch (\Throwable $ex)
+            {
+                App::getBean('ErrorLog')->onException($ex);
+            }
+        };
+
+        $this->worker->onError = function (ConnectionInterface $connection, int $code, string $msg) {
+            try
+            {
+                // @phpstan-ignore-next-line
+                $clientId = $connection->id;
+                RequestContext::muiltiSet([
+                    'server'   => $this,
+                    'clientId' => $clientId,
+                ]);
+                Event::trigger('IMI.WORKERMAN.SERVER.ERROR', [
+                    'server'     => $this,
+                    'clientId'   => $clientId,
+                    'connection' => $connection,
+                    'code'       => $code,
+                    'msg'        => $msg,
+                ], $this);
+                RequestContext::destroy();
+            }
+            catch (\Throwable $ex)
+            {
+                App::getBean('ErrorLog')->onException($ex);
+            }
+        };
+
+        $this->worker->onWorkerReload = function (Worker $worker) {
+            try
+            {
+                RequestContext::muiltiSet([
+                    'server' => $this,
+                    'worker' => $worker,
+                ]);
+                Event::trigger('IMI.WORKERMAN.SERVER.WORKER_RELOAD', [
+                    'server' => $this,
+                    'worker' => $worker,
+                ], $this);
+                RequestContext::destroy();
+            }
+            catch (\Throwable $ex)
+            {
+                App::getBean('ErrorLog')->onException($ex);
+            }
+        };
+
+        $this->worker->onWorkerStart = function (Worker $worker) {
+            try
+            {
+                // 随机数播种
+                mt_srand();
+
+                Imi::loadRuntimeInfo(Imi::getRuntimePath('runtime'));
+
+                // 创建共享 Worker 的服务
+                $config = $this->config;
+                if (!($config['shareWorker'] ?? false) && ($config['autorun'] ?? true))
+                {
+                    foreach (Config::get('@app.workermanServer') as $name => $config)
+                    {
+                        $shareWorker = $config['shareWorker'] ?? false;
+                        if (false !== $shareWorker && $this->getName() === $shareWorker)
+                        {
+                            /** @var IWorkermanServer $server */
+                            $server = ServerManager::createServer($name, $config);
+                            $subWorker = $server->getWorker();
+                            $subWorker->count = $worker->count;
+                            $subWorker->listen();
+                        }
+                    }
+                }
+
+                RequestContext::muiltiSet([
+                    'server' => $this,
+                    'worker' => $worker,
+                ]);
+
+                // 多进程通讯组件连接
+                $channel = Config::get('@app.workerman.channel');
+                if ($channel)
+                {
+                    Client::connect($channel['host'] ?: '127.0.0.1', $channel['port'] ?: 2206);
+                    // 监听进程通讯
+                    $callback = function (array $data) {
+                        $action = $data['action'] ?? null;
+                        if (!$action)
+                        {
+                            return;
+                        }
+                        Event::trigger('IMI.PIPE_MESSAGE.' . $action, [
+                            'data'      => $data,
+                        ]);
+                    };
+                    $workerId = ImiWorker::getWorkerId();
+                    Client::on('imi.process.message.' . $this->getName() . '.' . $workerId, $callback);
+                    Client::on('imi.process.message.' . $workerId, $callback);
+                }
+
+                if (isset($config['configs']['registerAddress']))
+                {
+                    Gateway::$registerAddress = $config['configs']['registerAddress'];
+                }
+
+                Event::trigger('IMI.WORKERMAN.SERVER.WORKER_START', [
+                    'server' => $this,
+                    'worker' => $worker,
+                ], $this);
+
+                \Imi\Worker::inited();
+                foreach (ServerManager::getServers() as $name => $_)
+                {
+                    Server::getInstance($name);
+                }
+                RequestContext::destroy();
+            }
+            catch (\Throwable $ex)
+            {
+                App::getBean('ErrorLog')->onException($ex);
+            }
+        };
+
+        $this->worker->onWorkerStop = function (Worker $worker) {
+            try
+            {
+                RequestContext::muiltiSet([
+                    'server' => $this,
+                    'worker' => $worker,
+                ]);
+                Event::trigger('IMI.WORKERMAN.SERVER.WORKER_STOP', [
+                    'server' => $this,
+                    'worker' => $worker,
+                ], $this);
+                RequestContext::destroy();
+            }
+            catch (\Throwable $ex)
+            {
+                App::getBean('ErrorLog')->onException($ex);
+            }
+        };
+    }
+
+    /**
+     * 获取客户端地址
+     *
+     * @param string|int $clientId
+     */
+    public function getClientAddress($clientId): IPEndPoint
+    {
+        /** @var TcpConnection|null $connection */
+        $connection = $this->worker->connections[$clientId] ?? null;
+        if (null === $connection)
+        {
+            throw new InvalidArgumentException(sprintf('Client %s does not exists', $clientId));
+        }
+
+        return new IPEndPoint($connection->getRemoteIp(), $connection->getRemotePort());
+    }
+
+    /**
+     * 获取实例化 Worker 用的协议.
+     */
+    abstract protected function getWorkerScheme(): string;
+}
---
+++ src/WorkermanApp.php
@@ -0,0 +1,168 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Imi\Workerman;
+
+use Imi\App;
+use Imi\Bean\Annotation;
+use Imi\Bean\Scanner;
+use Imi\Cli\CliApp;
+use Imi\Config;
+use Imi\Core\App\Enum\LoadRuntimeResult;
+use Imi\Event\Event;
+use Imi\Main\Helper;
+use Imi\Util\Imi;
+use Imi\Util\Process\ProcessAppContexts;
+use Symfony\Component\Console\ConsoleEvents;
+use Symfony\Component\Console\Event\ConsoleCommandEvent;
+use Symfony\Component\Console\Input\ArgvInput;
+
+class WorkermanApp extends CliApp
+{
+    /**
+     * 应用模式的配置.
+     */
+    protected array $appConfig = [
+        'Timer' => WorkermanTimer::class,
+        'beans' => [
+            'ServerUtil' => \Imi\Workerman\Server\Util\LocalServerUtil::class,
+        ],
+    ];
+
+    /**
+     * 构造方法.
+     */
+    public function __construct(string $namespace)
+    {
+        parent::__construct($namespace);
+        $this->cliEventDispatcher->addListener(ConsoleEvents::COMMAND, function (ConsoleCommandEvent $e) {
+            $this->onCommand($e);
+        }, \PHP_INT_MAX - 1000);
+        Event::one('IMI.SCAN_APP', function () {
+            $this->onScanApp();
+        });
+    }
+
+    /**
+     * 获取应用类型.
+     */
+    public function getType(): string
+    {
+        return 'workerman';
+    }
+
+    /**
+     * 加载配置.
+     */
+    public function loadConfig(bool $initDotEnv = true): void
+    {
+        parent::loadConfig(false);
+        foreach (Config::get('@app.workermanServer', []) as $name => $config)
+        {
+            // 加载服务器配置文件
+            foreach (Imi::getNamespacePaths($config['namespace']) as $path)
+            {
+                $fileName = $path . '/config/config.php';
+                if (is_file($fileName))
+                {
+                    Config::addConfig('@server.' . $name, include $fileName);
+                    break;
+                }
+            }
+        }
+        if ($initDotEnv)
+        {
+            $this->loadDotEnv();
+        }
+    }
+
+    /**
+     * 加载运行时.
+     */
+    public function loadRuntime(): int
+    {
+        $this->initRuntime();
+        $input = new ArgvInput();
+        $isServerStart = ('workerman/start' === ($_SERVER['argv'][1] ?? null));
+        if ($isServerStart)
+        {
+            $result = false;
+        }
+        else
+        {
+            // 尝试加载项目运行时
+            $appRuntimeFile = $input->getParameterOption('--app-runtime');
+            if (false !== $appRuntimeFile && Imi::loadRuntimeInfo($appRuntimeFile))
+            {
+                return LoadRuntimeResult::ALL;
+            }
+        }
+        // 尝试加载 imi 框架运行时
+        if ($file = $input->getParameterOption('--imi-runtime'))
+        {
+            // 尝试加载指定 runtime
+            $result = Imi::loadRuntimeInfo($file, true);
+        }
+        else
+        {
+            // 尝试加载默认 runtime
+            $result = Imi::loadRuntimeInfo(Imi::getRuntimePath('imi-runtime'), true);
+        }
+        if ($result)
+        {
+            return LoadRuntimeResult::IMI_LOADED;
+        }
+        else
+        {
+            // 不使用缓存时去扫描
+            Scanner::scanImi();
+            if ($isServerStart)
+            {
+                $imiRuntime = Imi::getRuntimePath('imi-runtime-bak');
+                Imi::buildRuntime($imiRuntime);
+                // 执行命令行生成缓存
+                $cmd = Imi::getImiCmd('imi/buildRuntime', [], [
+                    'imi-runtime' => $imiRuntime,
+                ]);
+                do
+                {
+                    passthru(\Imi\cmd($cmd), $code);
+                    $result = Imi::loadRuntimeInfo(Imi::getRuntimePath('runtime'));
+                    sleep(1);
+                } while (0 !== $code);
+
+                return LoadRuntimeResult::ALL;
+            }
+
+            return LoadRuntimeResult::IMI_LOADED;
+        }
+    }
+
+    /**
+     * 加载入口.
+     */
+    public function loadMain(): void
+    {
+        parent::loadMain();
+        foreach (Config::get('@app.workermanServer', []) as $name => $config)
+        {
+            Helper::getMain($config['namespace'], 'server.' . $name);
+        }
+    }
+
+    private function onCommand(ConsoleCommandEvent $e): void
+    {
+        App::set(ProcessAppContexts::MASTER_PID, getmypid(), true);
+    }
+
+    private function onScanApp(): void
+    {
+        $namespaces = [];
+        foreach (Config::get('@app.workermanServer', []) as $config)
+        {
+            $namespaces[] = $config['namespace'];
+        }
+        Annotation::getInstance()->initByNamespace($namespaces);
+    }
+}
