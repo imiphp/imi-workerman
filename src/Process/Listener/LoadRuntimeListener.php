a7013d01f3d4a57af874e636934c5caba04e077e
Yurun <admin@yurunsoft.com>
2021-06-25 14:56:42 +0800

    修复问题

---
+++ src/Process/Listener/LoadRuntimeListener.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Imi\Workerman\Process\Listener;
+
+use Imi\Config;
+use Imi\Event\EventParam;
+use Imi\Event\IEventListener;
+use Imi\Workerman\Process\ProcessManager;
+
+class LoadRuntimeListener implements IEventListener
+{
+    /**
+     * 事件处理方法.
+     */
+    public function handle(EventParam $e): void
+    {
+        $config = Config::get('@app.imi.runtime.workerman', []);
+        if (!($config['process'] ?? true))
+        {
+            return;
+        }
+        $data = $e->getData()['data']['workermanProcess'] ?? [];
+        ProcessManager::setMap($data['process'] ?? []);
+    }
+}
